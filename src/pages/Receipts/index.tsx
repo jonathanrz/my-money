f3c3b02f1ceb4fe325dca439cc6867146d2f6f45
Jonathan <jonathanrafaelz@gmail.com>
2024-02-20 17:44:35 -0300

    delete button

--- src/pages/Receipts/index.tsx
+++ src/pages/Receipts/index.tsx
@@ -1,16 +1,33 @@
 import { useMemo, useState } from "react";
 import dayjs, { Dayjs } from "dayjs";
 import { groupBy } from "lodash";
+import { useMutation, useQueryClient } from "react-query";
+import { Button } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import useAccountQuery from "../../hooks/useAccountQuery";
 import useReceiptsQuery from "../../hooks/useReceiptsQuery";
 import formatCurrency from "../../helpers/formatCurrency";
+import constants from "../../constants";
 
 export default function ReceiptsPage() {
+  const queryClient = useQueryClient();
   const [month, setMonth] = useState<Dayjs>(dayjs());
   const accountsAsync = useAccountQuery();
   const receiptsAsync = useReceiptsQuery(month);
+
+  const deleteReceipt = useMutation({
+    mutationFn: async (id: string) => {
+      await fetch(`${constants.URLS.receipts}/${id}`, {
+        method: "DELETE",
+      });
+
+      queryClient.invalidateQueries({
+        queryKey: constants.reactQueryKeyes.generateReceiptKey(month),
+      });
+    },
+  });
 
   const accounts = useMemo(
     () => groupBy(accountsAsync.data, "id"),
@@ -38,6 +55,24 @@ export default function ReceiptsPage() {
       type: "number",
       width: 130,
       valueFormatter: (params) => formatCurrency(params.value),
+    },
+    {
+      field: "id",
+      headerName: "Actions",
+      type: "number",
+      width: 200,
+      renderCell: (params) => (
+        <Button
+          variant="outlined"
+          startIcon={<DeleteIcon />}
+          onClick={() => {
+            if (confirm("Are you sure?"))
+              deleteReceipt.mutate(params.value as string);
+          }}
+        >
+          Delete
+        </Button>
+      ),
     },
   ];
 
